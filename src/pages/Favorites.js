af43eaffa5323e64a0a69b59ea4b1b655cd4000c
Lucas Couto <loc.unb@gmail.com>
2022-05-29 16:17:26 -0300

    Lists all favorites in favorites page

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -6,7 +6,20 @@ function FavoritesPage() {
   const favoritesContext = useContext(FavoritesContext);
   const favorites = favoritesContext.favorites;
 
-  return <MeetupList meetups={favorites} />;
+  let content;
+
+  if (favoritesContext.totalFavorites === 0) {
+    content = <p>You got no favorites yet. Start adding some?</p>;
+  } else {
+    content = <MeetupList meetups={favorites} />;
+  }
+
+  return (
+    <section>
+      <h1>My Favorites</h1>
+      {content}
+    </section>
+  );
 }
 
 export default FavoritesPage;
