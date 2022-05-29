4ba769c55d387d588144d41cd1e66ec562b9c570
Lucas Couto <loc.unb@gmail.com>
2022-05-29 12:05:07 -0300

    Navigate programatically  (useNavigate)

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,6 +1,10 @@
+import { useNavigate } from "react-router-dom";
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  const navigate = useNavigate();
+
   function addMeetupHandler(meetupData) {
     fetch(
       "https://react-meetup-app-7d585-default-rtdb.firebaseio.com/meetups.json",
@@ -11,7 +15,7 @@ function NewMeetupPage() {
           "Content-Type": "application/json",
         },
       }
-    );
+    ).then(() => navigate("/"));
   }
 
   return (
