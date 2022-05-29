842d6ffd33e702eb3b066dec4ec9afd1b472cc68
Lucas Couto <loc.unb@gmail.com>
2022-05-29 00:57:17 -0300

    Uses props.children

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
 import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
+import React from "react";
 
 function App() {
   return (
-    <div>
+    <React.Fragment>
       <MainNavigation />
-      <Routes>
-        <Route path="/" element={<AllMeetupsPage />} />
-        <Route path="/new-meetup" element={<NewMeetupPage />} />
-        <Route path="/favorites" element={<FavoritesPage />} />
-      </Routes>
-    </div>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<AllMeetupsPage />} />
+          <Route path="/new-meetup" element={<NewMeetupPage />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+        </Routes>
+      </Layout>
+    </React.Fragment>
   );
 }
 
